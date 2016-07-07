c843fd9b23c2de427d74e9e1ca9ef00eadd6e0ca
divyakalidindi <divyakalidindi@divyas-air.wireless.dartmouth.edu>
2016-07-07 09:41:32 -0400

    js workshop

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// const $ = require('jquery');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+setInterval(() => {
+  num++;
+  $('#main').html(`You've been on this page for ${num} seconds`);
+}, 1000);
